fc59d82bc791fb94dbb6d83f4fa039bd39c67038
Letitia Xu <letitia1995xx@gmail.com>
2016-05-30 14:24:41 +0800

    first day

---
+++ ContractCouple/ContractCouple/ContractCouple/MainViewController.h
@@ -0,0 +1,14 @@
+//
+//  MainViewController.h
+//  ContractCouple
+//
+//  Created by 张泽昕 on 16/5/30.
+//  Copyright © 2016年 张泽昕. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MainViewController : UITabBarController
+
+@end
---
+++ ContractCouple/ContractCouple/ContractCouple/MeViewController.m
@@ -0,0 +1,39 @@
+//
+//  MeViewController.m
+//  ContractCouple
+//
+//  Created by 张泽昕 on 16/5/30.
+//  Copyright © 2016年 张泽昕. All rights reserved.
+//
+
+#import "MeViewController.h"
+
+@interface MeViewController ()
+
+@end
+
+@implementation MeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.tabBarItem.selectedImage = [[UIImage imageNamed:@"menu_me_on"]
+                                  imageWithRenderingMode:UIImageRenderingModeAlwaysOriginal];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
